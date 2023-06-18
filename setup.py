c29d839713e872645181cd5110ee36a4fb543d9d
Evintkoo <99308658+Evintkoo@users.noreply.github.com>
2023-06-17 22:53:18 -0700

    remove test.ipynb

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="som-dna-clustering",
-    version="1.0.0",
+    version="0.1.0",
     description="A DNA clustering with implementation of Self Organizing Map",
     long_description=README,
     long_description_content_type="text/markdown",
